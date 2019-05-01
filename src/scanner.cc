78b4ca4f1019cc009fc21d36568672f5cec0688c
Jason Rudolph <jason@jasonrudolph.com>
2019-05-01 16:50:05 -0400

    Handle multi-line heredocs with spaces in delimiter
    Fixes the failing test introduced in 0d8adbc335.
    
    /xref https://github.com/tree-sitter/tree-sitter-bash/pull/47#discussion_r280183823

--- src/scanner.cc
+++ src/scanner.cc
@@ -88,7 +88,12 @@ struct Scanner {
 
   bool scan_heredoc_end_identifier(TSLexer *lexer) {
     current_leading_word.clear();
-    while ((iswalpha(lexer->lookahead) || iswspace(lexer->lookahead)) && current_leading_word.length() < heredoc_delimiter.length()) {
+    // Scan the first 'n' characters on this line, to see if they match the heredoc delimiter
+    while (
+      lexer->lookahead != '\0' &&
+      lexer->lookahead != '\n' &&
+      current_leading_word.length() < heredoc_delimiter.length()
+    ) {
       current_leading_word += lexer->lookahead;
       advance(lexer);
     }
